5bfb6cd2509ccbfad543f8095e239b441e818b0c
olig55 <oliver_gerling@biswashington.org>
2025-01-18 14:30:58 -0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hi")
+# 1) Creating a new file in Python
+with open("new_file.txt", "w") as file:
+  file.write("This is the first line of the new file.\n")
+
+# 2) Adding a line to the file
+with open("example.txt", "a") as file:
+  file.write("This is a new line of text.\n")
+
+# 3) Add a line to Python using user-input
+
+# 4) Printing out all the lines in a file
+
+# 5) Printing out specific characters in each line
+
+# 6) Outputting everything after a comma
+
+# 7) Deleting a specific line
+
+# 8) 
+
+# 9)
+
+# 10)
+
+
+
+
